5ada6de044b8f7f1e74fb6ca209dc42731a38a56
m-yoshiro <massugu.massugu@gmail.com>
2018-09-10 08:24:20 +0900

    Add sketch modules

--- src/my-command.js
+++ src/my-command.js
@@ -1,12 +1,14 @@
+const sketch = require('sketch/dom');
 const { prefix } = require('config');
 
 export default function(context) {
-  const selectedLayers = context.selection
-  const selectedCount = selectedLayers.length
+  const document = sketch.fromNative(context.document)
+  const selectedLayers = context.selection;
+  const selectedCount = selectedLayers.length;
 
   if (selectedCount === 0) {
-    context.document.showMessage('No layers are selected.')
+    context.document.showMessage('No layers are selected.');
   } else {
-    context.document.showMessage(`${selectedCount} layers selected.`)
+    context.document.showMessage(`${selectedCount} layers selected.`);
   }
 }
