252bbc9b39bbb4a9c39ee97439aa21ab89677d8f
m-yoshiro <massugu.massugu@gmail.com>
2018-09-13 08:01:05 +0900

    tokensArtboard > tokens

--- src/my-command.js
+++ src/my-command.js
@@ -3,10 +3,10 @@ const { prefix, artboardName } = require('./config');
 
 export default function(context) {
   const document = sketch.fromNative(context.document);
-  const tokensArtboard = document.getLayersNamed(artboardName);
+  const tokens = document.getLayersNamed(artboardName);
 
-  if (tokensArtboard.length > 0 ) {
-    context.document.showMessage(tokensArtboard[0].name);
+  if (tokens.length > 0 ) {
+    context.document.showMessage(tokens[0].name);
   } else {
     context.document.showMessage('not fond');
   }
