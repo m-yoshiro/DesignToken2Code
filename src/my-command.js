738e65854f5dbd4b025211cbf3a9444937e0cddd
m-yoshiro <massugu.massugu@gmail.com>
2018-09-17 12:20:51 +0900

    Cleanup code

--- src/my-command.js
+++ src/my-command.js
@@ -1,51 +1,53 @@
-const sketch = require('sketch/dom');
-const { UI } = require('sketch');
-const { prefix, artboardName } = require('./config');
-const { escapeRegExp } = require('./utils');
+const sketch = require('sketch/dom')
+const { UI } = require('sketch')
+const { prefix, artboardName } = require('./config')
+const { escapeRegExp } = require('./utils')
 
-const getTokenLayersByPattern = (layers, pattern) => {
-  return layers.filter( elm => (elm.type === `${sketch.Types.Shape}`) && pattern.test(elm.name) );
-};
+const getTokenLayersByPattern = (layers, pattern) =>
+  layers.filter(
+    elm => elm.type === `${sketch.Types.Shape}` && pattern.test(elm.name)
+  )
 
-const generateTokensFromLayers = (layers) => {
-  return layers.map( elm => {
-    return {
-      name: elm.name,
-      color: elm.style.fills[0].color,
-    };
-  });
-};
+const generateTokensFromLayers = layers =>
+  layers.map(elm => ({
+    name: elm.name,
+    color: elm.style.fills[0].color,
+  }))
 
 export default function(context) {
-  const document = sketch.fromNative(context.document);
-  const tokensArtboard = document.getLayersNamed(artboardName)[0];
-  const tokenNamePattern = new RegExp(`^${escapeRegExp(prefix)}`);
+  const document = sketch.fromNative(context.document)
+  const tokensArtboard = document.getLayersNamed(artboardName)[0]
+  const tokenNamePattern = new RegExp(`^${escapeRegExp(prefix)}`)
 
   if (!tokensArtboard.layers.length) {
-    UI.message('Not found');
-    return;
+    UI.message('Not found')
+    return
   }
 
-  const tokenLayers = getTokenLayersByPattern(tokensArtboard.layers, tokenNamePattern);
-  const tokens = generateTokensFromLayers(tokenLayers);
+  const tokenLayers = getTokenLayersByPattern(
+    tokensArtboard.layers,
+    tokenNamePattern
+  )
+  const tokens = generateTokensFromLayers(tokenLayers)
 
-  const message = tokens.map(elm => `${elm.name}: ${elm.color};\n` ).reduce( (a, b) => a + b );
+  const message = tokens
+    .map(elm => `${elm.name}: ${elm.color};\n`)
+    .reduce((a, b) => a + b)
 
   // Dialog
-  const dialog = NSAlert.alloc().init();
-  dialog.messageText = 'DesignTokens2Code';
-  dialog.informativeText = message;
+  const dialog = NSAlert.alloc().init()
+  dialog.messageText = 'DesignTokens2Code'
+  dialog.informativeText = message
 
-  dialog.addButtonWithTitle('Copy');
-  dialog.addButtonWithTitle('Close');
-  const buttons = dialog.buttons();
+  dialog.addButtonWithTitle('Copy')
+  dialog.addButtonWithTitle('Close')
 
   // https://github.com/skpm/dialog/blob/master/lib/message-box.js#L96
-  const response = Number(dialog.runModal()) - 1000;
+  const response = Number(dialog.runModal()) - 1000
 
   if (response === 0) {
     // Copy to clipboard
-    let pasteBoard = NSPasteboard.generalPasteboard()
+    const pasteBoard = NSPasteboard.generalPasteboard()
     pasteBoard.clearContents()
     pasteBoard.writeObjects([message])
 
