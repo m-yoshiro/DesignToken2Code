2e66101049995270d1958bf535ae742cc3cb218a
m-yoshiro <massugu.massugu@gmail.com>
2018-09-25 07:56:06 +0900

    Add validate method

---
+++ test/core/token.spec.js
@@ -0,0 +1,32 @@
+/* global describe:false, it:false */
+const { assert } = require('chai')
+
+const Token = require('../../src/core/token')
+/* eslint-disable no-unused-vars */
+describe('Token Class:', () => {
+  it('Should throw Error when luck "type" property', () => {
+    assert.throws(
+      () => {
+        const newToken = new Token({
+          format: 'scss',
+          name: 'token-name',
+          value: '#fff',
+        })
+      },
+      ReferenceError,
+      /Data must have type property/
+    )
+  })
+
+  it('Should throw TypeError', () => {
+    assert.throws(() => {
+      const newToken = new Token({
+        type: 87,
+        format: 'scss',
+        name: 'token-name',
+        value: '#fff',
+      })
+    }, TypeError)
+  })
+})
+/* eslint-enable no-unused-vars */
