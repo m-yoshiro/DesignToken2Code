36c14b60239feef41a68e6d4cfb7d8ec0f02bf95
m-yoshiro <massugu.massugu@gmail.com>
2018-09-25 22:42:38 +0900

    Add test of #output() method

--- test/core/token.spec.js
+++ test/core/token.spec.js
@@ -1,32 +1,47 @@
-/* global describe:false, it:false */
+/* global describe:false, it:false, context:false */
 const { assert } = require('chai')
 
 const Token = require('../../src/core/token')
 /* eslint-disable no-unused-vars */
 describe('Token Class:', () => {
-  it('Should throw Error when luck "type" property', () => {
-    assert.throws(
-      () => {
-        const newToken = new Token({
-          format: 'scss',
-          name: 'token-name',
-          value: '#fff',
-        })
-      },
-      ReferenceError,
-      /Data must have type property/
-    )
+  describe('#validate()', () => {
+    context('when luck "type" property', () => {
+      it('Should throw ReferenceError', () => {
+        assert.throws(
+          () => {
+            const newToken = new Token({
+              format: 'scss',
+              name: 'token-name',
+              value: '#fff',
+            })
+          },
+          ReferenceError,
+          /Data must have type property/
+        )
+      })
+    })
+
+    context('when prop type is not "string"', () => {
+      it('Should throw TypeError', () => {
+        assert.throws(() => {
+          const newToken = new Token({
+            type: 87,
+            format: 'scss',
+            name: 'token-name',
+            value: '#fff',
+          })
+        }, TypeError)
+      })
+    })
   })
 
-  it('Should throw TypeError when prop type is not "string"', () => {
-    assert.throws(() => {
-      const newToken = new Token({
-        type: 87,
-        format: 'scss',
-        name: 'token-name',
-        value: '#fff',
+  // TODO:
+  describe('#output()', () => {
+    context('when output tokens with "scss" format', () => {
+      it('Should output code is scss format', () => {
+        assert.isOk('everything is ok')
       })
-    }, TypeError)
+    })
   })
 })
 /* eslint-enable no-unused-vars */
