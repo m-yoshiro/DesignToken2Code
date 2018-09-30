d02addcafce442cbd1622e307784aa8d0126bf1c
m-yoshiro <massugu.massugu@gmail.com>
2018-10-01 08:06:40 +0900

    Integrated index.js

---
+++ src/ui/index.js
@@ -0,0 +1,29 @@
+module.export.createDialog = (context, title, message, buttons = []) => {
+  const dialog = NSAlert.alloc().init()
+  dialog.messageText = title
+  dialog.informativeText = message
+
+  if (Array.isArray(buttons) && buttons.length > 0) {
+    buttons.forEach(button => {
+      dialog.addButtonWithTitle(button.text)
+    })
+  }
+
+  // https://github.com/skpm/dialog/blob/master/lib/message-box.js#L96
+  const response = Number(dialog.runModal()) - 1000
+
+  if (
+    buttons[response].action &&
+    typeof buttons[response].action === 'function'
+  ) {
+    buttons[response].action()
+  }
+}
+
+module.exports.paseteBoardWrite = (context, data, message = 'Copied') => {
+  const pasteBoard = NSPasteboard.generalPasteboard()
+  pasteBoard.clearContents()
+  pasteBoard.writeObjects([data])
+
+  context.document.showMessage(message)
+}
