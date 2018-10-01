228a5cd876554a655ba7b64c163506d954ada204
m-yoshiro <massugu.massugu@gmail.com>
2018-10-01 10:01:07 +0900

    fix

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 module.exports.escapeRegExp = string =>
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
   string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
